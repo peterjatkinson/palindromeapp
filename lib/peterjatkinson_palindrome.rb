ebed520b1a49ce5390fffda4f66d9b5916fe0985
Peter Atkinson <peterjatkinson@gmail.com>
2021-07-20 17:08:13 +0100

    Initialise repository

---
+++ lib/peterjatkinson_palindrome.rb
@@ -0,0 +1,6 @@
+require "peterjatkinson_palindrome/version"
+
+module PeterjatkinsonPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/peterjatkinson_palindrome/version.rb
@@ -0,0 +1,3 @@
+module PeterjatkinsonPalindrome
+  VERSION = "0.1.0"
+end
